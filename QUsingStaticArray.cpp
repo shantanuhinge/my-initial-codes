cb117e079c329e91707a07d04fd84d6c7b606998
shantanuhinge <35631412+shantanuhinge@users.noreply.github.com>
2018-06-01 00:51:41 +0530

    Add files via upload

--- QUsingStaticArray.cpp
+++ QUsingStaticArray.cpp
@@ -56,11 +56,43 @@ void traverse()
 
 int main()
 {
-	insert(2);
-	insert(32);
-	insert(24);
-	insert(62);
-	traverse();
-	Delete();
-	traverse();
+	int a,b,c,p;
+	lable:
+	cout << endl;
+	cout << "enter q action" << endl << "1 insert "<<endl<< "2 delete " <<endl<<"3 traverse"<< endl;
+	cin >> a;
+	switch(a)
+	{
+		case 1: 
+			cout << "enter no of elements to be entered "<<endl;
+			cin >> b;
+			//cout << "fff "<<endl;
+			for(int i=0;i<b;i++)
+			{
+				cout << "enter element "<< i+1 << endl;
+				cin >> p;
+				insert(p);
+				//cout << "ggg "<<endl;
+			}
+		goto lable;
+		break;
+		
+		case 2:
+			cout << "enter no of elements to be deleted "<<endl;
+			cin >> c;
+			for(int j=0;j<c;j++)
+			{
+				cout << "deleting element "<<endl;
+				Delete();
+			}
+		goto lable;
+		break;
+		
+		case 3:
+			traverse();
+			break;
+		
+		default: 
+			cout <<"end"<<endl;
+	}
 }
