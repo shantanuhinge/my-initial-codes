6b056d4abe75041ffae57c60d8c891dd515e42e6
shantanuhinge <35631412+shantanuhinge@users.noreply.github.com>
2018-12-19 11:50:56 +0530

    Add files via upload

--- bst.cpp
+++ bst.cpp
@@ -13,12 +13,59 @@ struct node
 
 struct node *newNode(int data)
 {
-	node *temp = new node;
+	struct node *temp = new node;
 	temp->data = data;
 	temp->left = NULL;
 	temp->right = NULL;
 	return(temp);
 }
+
+void printPostorder(struct node *node) 
+{ 
+    if (node == NULL) 
+        return; 
+  
+    // first recur on left subtree 
+    printPostorder(node->left); 
+  
+    // then recur on right subtree 
+    printPostorder(node->right); 
+  
+    // now deal with the node 
+    cout << node->data << " "; 
+} 
+  
+/* Given a binary tree, print its nodes in inorder*/
+void printInorder(struct node* node) 
+{ 
+    if (node == NULL) 
+        return; 
+  
+    /* first recur on left child */
+    printInorder(node->left); 
+  
+    /* then print the data of node */
+    cout << node->data << " "; 
+  
+    /* now recur on right child */
+    printInorder(node->right); 
+} 
+  
+/* Given a binary tree, print its nodes in preorder*/
+void printPreorder(struct node* node) 
+{ 
+    if (node == NULL) 
+        return; 
+  
+    /* first print data of node */
+    cout << node->data << " "; 
+  
+    /* then recur on left sutree */
+    printPreorder(node->left);  
+  
+    /* now recur on right subtree */
+    printPreorder(node->right); 
+}  
 
 int main()
 {
@@ -27,7 +74,16 @@ int main()
 	root->right = newNode(1);	
 	root->left->left =newNode(0);
 	
-	cout << root->data<<" ";
+	    cout << "\nPreorder traversal of binary tree is \n"; 
+    printPreorder(root); 
+  
+    cout << "\nInorder traversal of binary tree is \n"; 
+    printInorder(root);  
+  
+    cout << "\nPostorder traversal of binary tree is \n"; 
+    printPostorder(root); 
+	
+	cout << endl<< endl<< root->data<<" ";
 	cout << root->left->data <<" ";
 	cout << root->right->data<<" ";
 	cout << root->left->left->data<<" ";	
